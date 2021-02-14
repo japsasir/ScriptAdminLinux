48d00ecb5232cbf55e317c84dda2dd049ac1da28
japsasir <japsasir@gmail.com>
2021-02-14 16:04:35 +0100

    Añadidas funcionalidades
    Creadas 1, 2, 3, 5, 6, 7 y 9
    Creada variable de menú
    
    Investigar 4, 8 pendiente.
    Refinar los comandos.

--- adminlinux.sh
+++ adminlinux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ###Este script está diseñado para un administrador de sistemas Linux a modo de documentación ejecutable.
+#Función de menú para usuario
 menu="
------------
+------------------
 Opciones:
     1-Crear usuario
     2-Habilitar usuario
@@ -13,9 +14,8 @@ Opciones:
     7-Espacio libre en disco
     8-Trazar ruta
     9-Salir    
------------
+------------------
 "
-#Función de menú para usuario
 
 echo "Bienvenido al menú de SysAdmin Linux."
 echo ""
@@ -28,8 +28,8 @@ date
 sleep 1
 read -p "$menu" opcion
 case $opcion in
-## Nota: Revisar como hacer el return, sin crear función a ser posible.
 #Menú de creación de usuario
+#Testado en repl.
 [1])
     clear
     echo
@@ -41,41 +41,61 @@ case $opcion in
     read descripcion
     groupadd $nuevogrupo
     useradd -g $nuevogrupo -c "$descripcion" $nuevousuario
-    echo "Nuevo usuario $nuevousuario creado para el grupo $nuevogrupo y con la siguiente descripción $descripcion ."
+    echo "Nuevo usuario <$nuevousuario> creado para el grupo: <$nuevogrupo> y con la siguiente descripción:  <$descripcion>."
     sleep 1s
     echo ""
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
+#Menú de habilitación de usuario
+#Testado en repl.
 [2])
-    read -p "Menú de habilitación de usuario. :" user
+    read -p "Menú de habilitación de usuario. Introduzca el nombre del usuario a habilitar:" user
     sleep 1s
+    passwd -u $user
+    echo "El usuario <$user> ha sido deshabilitado"
     echo ""
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
+#Menú de deshabilitación de usuario
+#Testado en repl.
 [3])
-    read -p "Menú de deshabilitación de usuario. :" user
+    read -p "Menú de deshabilitación de usuario. Introduzca el nombre del usuario a deshabilitar :" user
     sleep 1s
+    passwd -l $user
+    echo "El usuario <$user> ha sido deshabilitado"
     echo ""
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
+#Cambiar permisos a un usuario
+#Investigar sobre permisos específicos de usuario y los comandos correspondientes.
 [4])
-    read -p "Menú de copias de seguridad. :" user
+    echo "Aquí podrá cambiar los permisos que tiene un usuario sobre un fichero o directorio."
+    read -p "Primero Seleccione un usuario para cambiar sus permisos" user
     sleep 1s
+    read -p "Seleccione la ruta del archivo o directorio para modificar los permisos de <$user> sobre el mismo." ruta
+    echo "Los permisos para el usuario <$user> sobre el archivo o directorio <$ruta> son los siguientes"
+    read -p "Introduce los permisos que quieres asignar al usuario sobre este archivo o directorio, en formato numérico"
     echo ""
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
+#Menú de copia de seguridad del directorio de trabajo de usuario
+#Testear en máquina virtual.
 [5])
-    read -p "Menú de copia de seguridad del directorio de trabajo de usuario. :" user
+    read -p "Menú de copias de seguridad. Introduce un usuario para hacer una copia de seguridad de su directorio /home. :" user
     sleep 1s
+    echo "Creando copia de seguridad. Esto puede tardar algún tiempo..."
+    tar -zcvpf /backup/$user-$(date +%d-%m-%Y).tar.gz /home/$user
+    echo "La copia de seguridad ha sido creada en /backup/$user"
     echo ""
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
-    #Usuarios Online
+#Usuarios Online.
+#Revisar comando en máquina virtual. Debería valer.
 [6])
     echo "Estos son los usuarios conectados actualmente:"
     who -H
@@ -84,13 +104,18 @@ case $opcion in
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
+#Menú de espacio libre en disco.
+#Investigar si se puede concretar más
 [7])
-    read -p "Menú de espacio libre en disco:" user
+    echo "Aquí podrá ver un resumen del uso del disco. La cantidad libre será mostrado bajo la columna <Available>" 
+    df -h
     sleep 1s
     echo ""
     echo "Elige otra opción."
     read -p "$menu" opcion
     ;;
+#Menú de trazado de rutas ip o url
+#Traceroute?
 [8])
     read -p "Menú de trazado de rutas ip o url :" user
     sleep 1s
